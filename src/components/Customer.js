41a585bfd44c6d0f58413abcff419ee277370620
Eve Le <nguyenthao.lee@gmail.com>
2019-12-16 15:54:00 -0800

    Updated indentation

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Customer = (props) => {
-    return (
-       <section>
-           <p>Customer: {props.customerInfo.name}</p>
-           <p>Member since: {props.customerInfo.register_at}</p>
-           <p>Phone number: {props.customerInfo.phone}</p>
-           <section>
-               <p>Address: </p>
-               <p>{props.customerInfo.address}</p>
-               <p>{props.customerInfo.city}, {props.customerInfo.state}</p>
-               <p>{props.customerInfo.postal_code}</p>
-           </section>
-           <p>Movies checked out: {props.customerInfo.movies_checked_out_count}</p>
-           <p>Available credit: {props.customerInfo.account_credit}</p>
-       </section> 
-    );
+  return (
+    <section>
+      <p>Customer: {props.customerInfo.name}</p>
+      <p>Member since: {props.customerInfo.register_at}</p>
+      <p>Phone number: {props.customerInfo.phone}</p>
+      <section>
+        <p>Address: </p>
+        <p>{props.customerInfo.address}</p>
+        <p>{props.customerInfo.city}, {props.customerInfo.state}</p>
+        <p>{props.customerInfo.postal_code}</p>
+      </section>
+      <p>Movies checked out: {props.customerInfo.movies_checked_out_count}</p>
+      <p>Available credit: {props.customerInfo.account_credit}</p>
+    </section>
+  );
 }
 
 export default Customer;
--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Customer from './Customer.js';
 
 class CustomerList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customers: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: [],
     }
+  }
 
-    render() {
-        const customers = this.props.customers.map((customer, i) => {
-            return (
-                <section key={i}>
-                    <Customer customerInfo={customer}></Customer>
-                </section>
-            );
-        })
-        return <section>{customers}</section>;
-    }
+  render() {
+    const customers = this.props.customers.map((customer, i) => {
+      return (
+        <section key={i}>
+          <Customer customerInfo={customer}></Customer>
+        </section>
+      );
+    })
+    return <section>{customers}</section>;
+  }
 }
 
 export default CustomerList;

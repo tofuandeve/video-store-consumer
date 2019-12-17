8df47e37487e911a476a82c13c414073d0c27bb8
Eve Le <nguyenthao.lee@gmail.com>
2019-12-16 16:30:29 -0800

    Updated CustomerList.js to use data from API

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import Customer from './Customer.js';
-import customerData from './customer-data.json';
+import axios from 'axios';
 
 class CustomerList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       customers: [],
+      selectedCustomer: '',
     }
   }
 
+  selectCustomer = (customerId) => {
+    const customer = this.state.customers.find((customer) => {
+      return customer.id === customerId
+    });
+    this.setState({
+      selectedCustomer: customer,
+    });
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:3000/customers"
+    axios.get(url)
+      .then((response) => {
+        const customerData = response.data;
+        this.setState({
+          customers: customerData,
+          selectedCustomer: '',
+          error: '',
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message,
+        });
+      });
+  }
+
   render() {
-    const customers = customerData.map((customer, i) => {
+    const customers = this.state.customers.map((customer, i) => {
       return (
         <section key={i}>
-          <Customer customerInfo={customer}></Customer>
+          <Customer
+            customerInfo={customer}
+            selectCustomerCallBack={this.selectCustomer}
+          ></Customer>
         </section>
       );
     })

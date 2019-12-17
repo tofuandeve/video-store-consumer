2e8a8bf98fe3071a19e46d6e8d8a4854def5502d
gracexinran <gracexinran@gmail.com>
2019-12-16 16:20:27 -0800

    implemented Movie component for an individual movie

---
+++ src/components/Movie.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Movie = (props) => {
+  const { id, title, overview, releaseDate, imageUrl, externalId, selectMovieCallback } = props;
+
+  return (
+    <div className="card movie-card">
+
+      <section className="movie-card--header">
+        {id} - {title} - {releaseDate}
+        <button
+          className="btn btn-primary movie-card--select-movie-btn"
+          onClick={() => { selectMovieCallback(id) }}
+        >
+          Select
+        </button>
+      </section>
+
+      <section className="movie-card--body">
+        {imageUrl}
+        <p>{overview}</p>
+      </section>
+
+      <section className="movie-card--footer">
+        {externalId}
+      </section>
+
+    </div>
+  );
+};
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  overview: PropTypes.string,
+  imageUrl: PropTypes.string,
+  externalId: PropTypes.string,
+  selectMovieCallback: PropTypes.func
+}
+
+export default Movie;

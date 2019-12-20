184115973eaa5b01d68fcf2b19a02f11e8ccd5e4
gracexinran <gracexinran@gmail.com>
2019-12-19 18:04:45 -0800

    formatted home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomerList from './components/CustomerList';
@@ -65,13 +64,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to our video store!</h1>
-        </header>
-
-        <p className="App-intro"></p>
-
         <Router>
           <div>
             <nav>
